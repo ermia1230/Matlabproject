74b689357d47a6efcd904b68ddeb9f5597bcb461
Ermia Ghaffari <ermia@kth.se>
2024-02-03 17:41:34 +0100

    2d done

---
+++ lab1/uppgift2/2d.m
@@ -0,0 +1,5 @@
+// Kvadratisk konvergens för en iterativ metod definieras som ett förhållande mellan nuvarande updateringsterm (tn) och dess föregående.
+// Vad den säger är hur updateringstermen skiljer sig med varje iteration, alltså ifall vi har en kvadratisk konvergens eller inte.
+// Eftersom K är en division mellan nuvarande tn och föregående, så bör tn dubblas med varje iteration, vilket kommer resultera i att K bär ter sig om en konstant,
+// alltså vill vi inte att K skiljer så mycket med varje iteration.
+// Avslutningsvis bör även tn minskas tills den går mot noll i varje iteration.
